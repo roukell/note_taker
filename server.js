f9af56cc9cceae56dbcfd1cf79d89c567458c523
roukell <62136980+roukell@users.noreply.github.com>
2020-07-28 23:48:14 +1000

    added get and post

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = new express();
+const dbjson = require("./db/db.json")
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+// html routes
 app.get("/", (req, res) => {
     res.render("index");
 });
@@ -12,7 +17,19 @@ app.get("/notes", (req, res) => {
     res.render("notes");
 });
 
-app.listen(3000, () => {
-    console.log("App listening on port 3000")
+// api get routes
+app.get("/api/notes", (req, res) => {
+    res.json(dbjson);
+    console.log(dbjson);
+});
+
+// api post routes
+app.post("/api/notes", (req, res) => {
+    res.json(dbjson);
+    console.log(dbjson);
+});
+
+app.listen(PORT, () => {
+    console.log("App listening on port: " + PORT)
 })
 
