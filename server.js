2eb46a40861cbacfe635495dc0d8d4441f1d31e7
roukell <62136980+roukell@users.noreply.github.com>
2020-07-29 00:33:35 +1000

    updated corrected files

--- server.js
+++ server.js
@@ -1,33 +1,32 @@
 const express = require("express");
 const app = new express();
+const path = require("path");
 const dbjson = require("./db/db.json")
 const PORT = process.env.PORT || 8080;
 
 app.use(express.static("public"));
-app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // html routes
 app.get("/", (req, res) => {
-    res.render("index");
+    res.sendFile(path.resolve(__dirname, "index.html"));
 });
 
 app.get("/notes", (req, res) => {
-    res.render("notes");
+    res.sendFile(path.resolve(__dirname, "public/notes.html"));
 });
 
 // api get routes
 app.get("/api/notes", (req, res) => {
     res.json(dbjson);
-    console.log(dbjson);
 });
 
 // api post routes
 app.post("/api/notes", (req, res) => {
     res.json(dbjson);
-    console.log(dbjson);
 });
+
 
 app.listen(PORT, () => {
     console.log("App listening on port: " + PORT)
