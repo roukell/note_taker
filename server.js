3cb3215a79527467bf22837b6df88b40f3e3c58b
roukell <62136980+roukell@users.noreply.github.com>
2020-07-29 18:03:35 +1000

    update post api with unique id for each post

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = new express();
+const fs = require("fs");
 const path = require("path");
-const dbjson = require("./db/db.json")
+const dbjson = require("./db/db.json");
+const { v4: uuidv4 } = require('uuid');
 const PORT = process.env.PORT || 8080;
 
 app.use(express.static("public"));
@@ -9,10 +11,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // html routes
-app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "index.html"));
-});
-
 app.get("/notes", (req, res) => {
     res.sendFile(path.resolve(__dirname, "public/notes.html"));
 });
@@ -23,13 +21,30 @@ app.get("/api/notes", (req, res) => {
 });
 
 // api post routes
-app.post("/notes", (req, res) => {
+app.post("/api/notes", (req, res) => {
+    req.body.id = uuidv4();
     dbjson.push(req.body);
+    writeToFile("./db/db.json", JSON.stringify(dbjson));
     console.log(dbjson);
+    res.json(dbjson);
 });
 
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "public/index.html"));
+});
 
 app.listen(PORT, () => {
     console.log("App listening on port: " + PORT)
 })
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+      if (err) {
+        throw err;
+      }
+      console.log("Successful");
+    });
+  }
+
+
 
